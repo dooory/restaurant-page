dd005a4f80b2afeb3bd5f230ee9e6d4a10c3dc70
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 12:59:35 +1300

    add clearing content before loading page

--- src/index.js
+++ src/index.js
@@ -10,18 +10,26 @@ const menuButton = d.querySelector(".menu-button");
 const aboutButton = d.querySelector(".about-button");
 const contactButton = d.querySelector(".contact-button");
 
+function clearContent() {
+    content.replaceChildren();
+}
+
 homeButton.addEventListener("click", (e) => {
+    clearContent();
     home(content);
 });
 
 menuButton.addEventListener("click", (e) => {
+    clearContent();
     menu(content);
 });
 
 aboutButton.addEventListener("click", (e) => {
+    clearContent();
     about(content);
 });
 
 contactButton.addEventListener("click", (e) => {
+    clearContent();
     contact(content);
 });
