79010e3f978b31f29b9eeb36469bfd172c7bafe0
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 12:20:10 +1300

    add homepage

---
+++ src/homepage.js
@@ -0,0 +1,41 @@
+const d = document;
+
+export default function homepage(content) {
+    const homepageDiv = d.createElement("div");
+    homepageDiv.classList.add("homepage");
+
+    content.append(homepageDiv);
+
+    const [taglineDiv, taglineText] = tagline(homepageDiv);
+    const [descriptionDiv, descriptionHeader, descriptionText] =
+        description(homepageDiv);
+}
+
+function tagline(parentDiv) {
+    const taglineDiv = d.createElement("div");
+    taglineDiv.classList.add("tagline");
+    parentDiv.append(taglineDiv);
+
+    const taglineText = d.createElement("h3");
+    taglineText.textContent = "This is our motto!";
+    taglineDiv.append(taglineText);
+
+    return [taglineDiv, taglineText];
+}
+
+function description(parentDiv) {
+    const descriptionDiv = d.createElement("div");
+    descriptionDiv.classList.add("description");
+    parentDiv.append(descriptionDiv);
+
+    const descriptionHeader = d.createElement("h2");
+    descriptionHeader.textContent = "What our restaurant is about!";
+    descriptionDiv.append(descriptionHeader);
+
+    const descriptionText = d.createElement("p");
+    descriptionText.textContent =
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nisi expedita consequatur obcaecati reprehenderit excepturi debitis laboriosam aspernatur distinctio rerum rem blanditiis quidem, fuga consectetur quisquam totam ex commodi eos autem modi ipsum nihil ut voluptatibus quos. Laborum in dolorem quisquam saepe, quas, molestias dolorum expedita assumenda optio, tempora nulla.";
+    descriptionDiv.append(descriptionText);
+
+    return [descriptionDiv, descriptionHeader, descriptionText];
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Hello World!");
+import homepage from "./homepage.js";
+
+homepage(content);
