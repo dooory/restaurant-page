fa440cd43332c65758116be177b8d4e36b81f049
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 14:19:34 +1300

    add opening hours + address

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,15 @@
 const d = document;
 
+const storeHours = {
+    Sunday: ["8 am", "5 pm"],
+    Monday: ["8 am", "6 pm"],
+    Tuesday: ["8 am", "6 pm"],
+    Wednesday: ["8 am", "6 pm"],
+    Thursday: ["8 am", "7 pm"],
+    Friday: ["8 am", "7 pm"],
+    Saturday: ["8 am", "7 pm"],
+};
+
 export default function homepage(content) {
     const homepageDiv = d.createElement("div");
     homepageDiv.classList.add("home-page");
@@ -8,12 +18,8 @@ export default function homepage(content) {
 
     const [descriptionDiv, descriptionHeader, descriptionText] =
         description(homepageDiv);
-}
-
-function tagline(parentDiv) {
-    const taglineDiv = d.createElement("div");
-    taglineDiv.classList.add("tagline");
-    parentDiv.append(taglineDiv);
+    const [openingDiv, openingHeader, hoursList] = openingHours(homepageDiv);
+    const [locationDiv, locationHeader, locationText] = location(homepageDiv);
 
     return homepageDiv;
 }
@@ -24,7 +30,7 @@ function description(parentDiv) {
     parentDiv.append(descriptionDiv);
 
     const descriptionHeader = d.createElement("h2");
-    descriptionHeader.textContent = "A break away from reality";
+    descriptionHeader.textContent = '"A break away from reality"';
     descriptionDiv.append(descriptionHeader);
 
     const descriptionText = d.createElement("p");
@@ -33,4 +39,46 @@ function description(parentDiv) {
     descriptionDiv.append(descriptionText);
 
     return [descriptionDiv, descriptionHeader, descriptionText];
+}
+
+function openingHours(parentDiv) {
+    const openingDiv = d.createElement("div");
+    openingDiv.classList.add("opening-hours");
+    parentDiv.append(openingDiv);
+
+    const openingHeader = d.createElement("h2");
+    openingHeader.textContent = "Opening Hours";
+    openingDiv.append(openingHeader);
+
+    const hoursList = d.createElement("ul");
+    openingDiv.append(hoursList);
+
+    for (const day in storeHours) {
+        const dayHours = storeHours[day];
+        const dayStart = dayHours[0];
+        const dayEnd = dayHours[1];
+
+        const dayItem = d.createElement("li");
+        dayItem.textContent = `${day}: ${dayStart}–${dayEnd}`;
+
+        hoursList.append(dayItem);
+    }
+
+    return [openingDiv, openingHeader, hoursList];
+}
+
+function location(parentDiv) {
+    const locationDiv = d.createElement("div");
+    locationDiv.classList.add("location");
+    parentDiv.append(locationDiv);
+
+    const locationHeader = d.createElement("h2");
+    locationHeader.textContent = "Our Address";
+    locationDiv.append(locationHeader);
+
+    const locationText = d.createElement("p");
+    locationText.textContent = "56 Lancy Street, Porkerville";
+    locationDiv.append(locationText);
+
+    return [locationDiv, locationHeader, locationText];
 }
