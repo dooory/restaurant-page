b79072485fb0b5bdc78353c85d2fd7c488b37039
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 15:51:56 +1300

    add currentPage variable and setting default page to home

--- src/index.js
+++ src/index.js
@@ -11,23 +11,34 @@ const menuButton = d.querySelector(".menu-button");
 const aboutButton = d.querySelector(".about-button");
 const contactButton = d.querySelector(".contact-button");
 
+let currentPage = "home";
+
+pageTitle.textContent = "Home";
+home(content);
+
 function clearContent() {
     content.replaceChildren();
 }
 
 homeButton.addEventListener("click", (e) => {
+    currentPage = "home";
+
     pageTitle.textContent = "Home";
     clearContent();
     home(content);
 });
 
 menuButton.addEventListener("click", (e) => {
+    currentPage = "menu";
+
     pageTitle.textContent = "Our Menu";
     clearContent();
     menu(content);
 });
 
 contactButton.addEventListener("click", (e) => {
+    currentPage = "contact";
+
     pageTitle.textContent = "Contact Us";
     clearContent();
     contact(content);
