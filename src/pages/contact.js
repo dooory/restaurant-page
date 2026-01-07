4cdb37e52146da1e9aed5be7efd42707bda7fd48
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 15:46:48 +1300

    add contact page contents

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,25 @@
 const d = document;
 
-export default function contactPage(content) {
-    const contactPageDiv = d.createElement("div");
-    contactPageDiv.classList.add("contact-page");
+const details = {
+    phone: "222-222-444",
+    email: "our.restaurant@easymail.com",
+};
 
-    content.append(contactPageDiv);
+export default function contactPage(content) {
+    const page = d.createElement("div");
+    page.classList.add("contact-page");
+
+    const container = d.createElement("div");
+    container.classList.add("contact-container");
+
+    const phoneText = d.createElement("p");
+    phoneText.textContent = `Phone: ${details.phone}`;
+
+    const emailText = d.createElement("p");
+    emailText.textContent = `Email: ${details.email}`;
+
+    container.append(phoneText);
+    container.append(emailText);
+    page.append(container);
+    content.append(page);
 }
