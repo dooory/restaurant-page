d5d6e432dc8e104f2d8febc97584e73a176d4317
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 14:33:37 +1300

    add title for each page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import menu from "./pages/menu.js";
 
 const d = document;
 
+const pageTitle = d.querySelector("#pageHeader .title");
+
 const homeButton = d.querySelector(".home-button");
 const menuButton = d.querySelector(".menu-button");
 const aboutButton = d.querySelector(".about-button");
@@ -14,16 +16,19 @@ function clearContent() {
 }
 
 homeButton.addEventListener("click", (e) => {
+    pageTitle.textContent = "Home";
     clearContent();
     home(content);
 });
 
 menuButton.addEventListener("click", (e) => {
+    pageTitle.textContent = "Our Menu";
     clearContent();
     menu(content);
 });
 
 contactButton.addEventListener("click", (e) => {
+    pageTitle.textContent = "Contact Us";
     clearContent();
     contact(content);
 });
